269eb3fdb2a692bd852339dbc46af2eed411944a
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-09-13 10:05:14 -0400

    wip: init tokenization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{
+    fs,
+    io::{self, BufRead, BufReader},
+    path,
+};
+
+mod token;
+
 fn main() {
-    println!("Hello, world!");
+    run_file("input/hello_world.lox");
+}
+
+fn run_file(path: &str) {
+    match fs::read_to_string(path) {
+        Ok(source) => {
+            token::tokenize(source);
+        }
+        Err(err) => println!("{:?}", err),
+    }
 }
